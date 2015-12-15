a863b4cc3538191f9e988eb38c7637028485ec4e
Cloud617 <Aronwong95@gmail.com>
2015-12-15 16:31:20 -0600

    I edit the mysite/settings.py
     I add 'polls', to the installed_Apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 )
-
-MIDDLEWARE_CLASSES = (
+IDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
